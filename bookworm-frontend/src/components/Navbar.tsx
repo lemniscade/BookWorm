5dc62448620436ba509bee655d8b6d1ccd5547f0
Mehmet Emin Meşe <memin1598@gmail.com>
2024-12-25 22:18:08 +0300

    navbar component updated

--- bookworm-frontend/src/components/Navbar.tsx
+++ bookworm-frontend/src/components/Navbar.tsx
@@ -6,88 +6,32 @@ import "../styles/Navbar.scss";
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [hamburgerVisible, sethamburgerVisible] = useState(true);//düzelt
 
   const toggleMenu = () => {
     setMenuOpen((prev) => !prev);
+    sethamburgerVisible((prev) => !prev);
   };
-  if (menuOpen) {
-    return (
-      <header className="navbar-container">
-        <GridRow columns={3}>
-          {/* Sol taraf: Logo */}
-          <GridColumn>
-            <div className="navbar-logo">MyLogo</div>
-          </GridColumn>
 
-          {/* Orta taraf: Menü */}
-          <GridColumn>
-            <GridColumn style={`navbar-menu open`} >
-              <GridRow>
-                <GridRow>
-                  <a href="#home">Home</a>
-                </GridRow>
-                <GridRow>
-                  <a href="#about">About</a>
-                </GridRow>
-                <GridRow>
-                  <a href="#services">Services</a>
-                </GridRow>
-                <GridRow>
-                  <a href="#contact">Contact</a>
-                </GridRow>
-              </GridRow>
-            </GridColumn>
-          </GridColumn>
-
-
-
-          {/* Sağ taraf: Hamburger ve Butonlar */}
-          <GridColumn>
-            <GridColumn style="navbar-buttons">
-              <button className="hamburger-menu" onClick={toggleMenu}>
-                ☰
-              </button>
-              <GridColumn style="desktop-buttons">
-                <button className="btn-login">Login</button>
-                <button className="btn-signup">Sign Up</button>
-              </GridColumn>
-            </GridColumn>
-          </GridColumn>
-        </GridRow>
-      </header>
-    )
-
-  } else {
-    return (<header className="navbar-container">
-      <GridRow columns={3}>
+  return (
+    <header className="navbar-container">
+      <GridRow columns={2}>
         {/* Sol taraf: Logo */}
-        <GridColumn>
+        {/* <GridColumn>
           <div className="navbar-logo">MyLogo</div>
-        </GridColumn>
-
+        </GridColumn> */}
         {/* Orta taraf: Menü */}
         <GridColumn>
-          <GridColumn style={`navbar-menu`} >
-            <GridColumn>
-              <GridColumn>
-                <a href="#home">Home</a>
-              </GridColumn>
-              <GridColumn>
-                <a href="#about">About</a>
-              </GridColumn>
-              <GridColumn>
-                <a href="#services">Services</a>
-              </GridColumn>
-              <GridColumn>
-                <a href="#contact">Contact</a>
-              </GridColumn>
-            </GridColumn>
+          <GridColumn style={`topnav${menuOpen ? " responsive" : ""}`} >
+            <a href="#home" className="active">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
           </GridColumn>
         </GridColumn>
-
         {/* Sağ taraf: Hamburger ve Butonlar */}
         <GridColumn>
-          <GridColumn style="navbar-buttons">
+          <GridColumn style={`navbar-buttons ${hamburgerVisible ? " visibility" : ""}`}>
             <button className="hamburger-menu" onClick={toggleMenu}>
               ☰
             </button>
@@ -98,9 +42,8 @@ function Navbar() {
           </GridColumn>
         </GridColumn>
       </GridRow>
-    </header>)
-  }
-
+    </header>
+  );
 }
 
 export default Navbar;
