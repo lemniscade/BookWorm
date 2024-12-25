c56e54e2457ddf2cb342cfbdd2b9202e53093c93
Mehmet Emin Meşe <memin1598@gmail.com>
2024-12-25 20:46:27 +0300

    navbar comment lines deleted.

--- bookworm-frontend/src/components/Navbar.tsx
+++ bookworm-frontend/src/components/Navbar.tsx
@@ -21,14 +21,6 @@ function Navbar() {
 
           {/* Orta taraf: Menü */}
           <GridColumn>
-            {/* <nav className={`navbar-menu ${menuOpen ? "open" : ""}`}>
-            <ul>
-              <li><a href="#home">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#services">Services</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-          </nav> */}
             <GridColumn style={`navbar-menu open`} >
               <GridRow>
                 <GridRow>
@@ -51,15 +43,6 @@ function Navbar() {
 
           {/* Sağ taraf: Hamburger ve Butonlar */}
           <GridColumn>
-            {/* <div className="navbar-buttons">
-            <button className="hamburger-menu" onClick={toggleMenu}>
-              ☰
-            </button>
-            <div className="desktop-buttons">
-              <button className="btn-login">Login</button>
-              <button className="btn-signup">Sign Up</button>
-            </div>
-          </div> */}
             <GridColumn style="navbar-buttons">
               <button className="hamburger-menu" onClick={toggleMenu}>
                 ☰
@@ -104,15 +87,6 @@ function Navbar() {
 
         {/* Sağ taraf: Hamburger ve Butonlar */}
         <GridColumn>
-          {/* <div className="navbar-buttons">
-            <button className="hamburger-menu" onClick={toggleMenu}>
-              ☰
-            </button>
-            <div className="desktop-buttons">
-              <button className="btn-login">Login</button>
-              <button className="btn-signup">Sign Up</button>
-            </div>
-          </div> */}
           <GridColumn style="navbar-buttons">
             <button className="hamburger-menu" onClick={toggleMenu}>
               ☰
